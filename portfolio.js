56c3a61b04ae486d35a79ea463b7d09044cbaf10
BouzirJawad <jawadbouzir01@gmail.com>
2025-01-09 17:55:59 +0100

    first commit, created a navbar

---
+++ portfolio.js
@@ -0,0 +1,17 @@
+
+  $(document).ready(function () {
+    const backToTop = $("#backToTop");
+
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 200) {
+        backToTop.fadeIn();
+      } else {
+        backToTop.fadeOut();
+      }
+    });
+
+    backToTop.click(function () {
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false; 
+    });
+  });
